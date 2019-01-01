051e285524473d062967a768618f18c9338bf216
rokusyou <rokusyou.ueki@gmail.com>
2019-01-01 14:07:33 +0900

    Added http error & write_log

--- Crawl_HS.py
+++ Crawl_HS.py
@@ -1,13 +1,32 @@
 from bs4 import BeautifulSoup
 import urllib.request as req
+import urllib
 from time import sleep
 import re
 import csv
+import datetime
+##################################
+#       SETTINGS
+##################################
+
+date = datetime.datetime.now()
+tstamp = '%04d%02d%02d' % (date.year, date.month ,date.day)
+
+OUT_F = 'output/HydroStations_%s.csv' % tstamp
+LOG_F  = r".\log\log%s.log" % tstamp
+BASE_URL = "http://fccj.jp/hystation/"
+err_url = "https://baseball.yahoo.co.jp/mlbdfadf/"
 
 ###################################
 #       METHODS
 ###################################
 
+	
+def write_log(file,str):
+	f_logout = open(file,'a')
+	f_logout.writelines(str + "\n")
+	f_logout.close()
+	
 def write_info(f_name,info_list):
 	
 	f_out = open(f_name,'a')
@@ -43,14 +62,19 @@ def get_geopoint(soup):
 			return st_lat,st_lon
 
 
-
-##################################
-#       SETTINGS
-##################################
-
-f_out = 'output/chain_info.csv'
-url = "http://fccj.jp/hystation/"
-
+def get_soup(url):
+	
+	try:
+		res = req.urlopen(url)
+		soup = BeautifulSoup(res,'html.parser')
+		return soup
+		
+	except urllib.error.HTTPError as http_e:
+		print( http_e.code , http_e.reason, url)
+		log_str = str( http_e.code) + http_e.reason +  url
+		write_log(LOG_F, str(http_e.code)  )
+	except Exception as e:
+		print(e)
 
 
 ##################################
@@ -59,9 +83,9 @@ url = "http://fccj.jp/hystation/"
 
 
 if __name__ == '__main__':
-	
-	res = req.urlopen(url)
-	soup = BeautifulSoup(res, 'html.parser')
+
+	soup = get_soup(err_url)
+	soup = get_soup(BASE_URL)
 	
 	for tr in soup.findAll('tr'):
 		
@@ -71,11 +95,11 @@ if __name__ == '__main__':
 
 			else:
 				
-				st_url = url + a_tag.get("href")
+				st_url = BASE_URL + a_tag.get("href")
 				st_name = a_tag.get_text()
 				
-				st_res = req.urlopen(st_url)
-				st_soup = BeautifulSoup(st_res,'html.parser')
+				
+				st_soup = get_soup(st_url)
 				
 				st_address= get_address(st_soup)
 				
@@ -83,9 +107,9 @@ if __name__ == '__main__':
 				
 				info_list = [st_name,st_address,st_lat,st_lon,st_url]
 				print(info_list)
-				write_info(f_out,info_list)
+				write_info(OUT_F,info_list)
 				
 				sleep(1.5)
 			
-
+	
 
